a99e43e25e832284bc0d336b019baeea94884092
David Löwenfels <282+dfl@users.noreply.github.com>
2026-02-05 16:26:37 +0000

    alias methods without swe_ prefix

--- lib/swisseph.rb
+++ lib/swisseph.rb
@@ -6,5 +6,14 @@ require 'swisseph/swisseph'
 Swe4r = Swisseph unless defined?(Swe4r)
 
 module Swisseph
-  # Modern methods or Ruby-level extensions can go here
+  class << self
+    # Automatically create shorthand aliases without the 'swe_' prefix
+    # e.g. Swisseph.julday instead of Swisseph.swe_julday
+    singleton_methods.each do |method_name|
+      next unless method_name.to_s.start_with?('swe_')
+
+      alias_name = method_name.to_s.delete_prefix('swe_')
+      alias_method alias_name, method_name unless respond_to?(alias_name)
+    end
+  end
 end
